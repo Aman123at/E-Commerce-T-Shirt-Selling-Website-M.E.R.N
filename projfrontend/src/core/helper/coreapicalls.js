64b5f2bfb2c1149da0f6624ff63288f361e00083
Aman <ammitiwari1234@gmail.com>
2021-01-02 12:05:14 +0530

    First Commit

---
+++ projfrontend/src/core/helper/coreapicalls.js
@@ -0,0 +1,16 @@
+import { API } from "../../backend.js";
+
+export const getProducts = () => {
+    return fetch(`${API}/products`, {
+            method: "GET"
+        })
+        .then(response => {
+            console.log(response)
+            return response.json();
+
+
+        })
+        .catch(err => console.log(err))
+}
+
+console.log(API)
